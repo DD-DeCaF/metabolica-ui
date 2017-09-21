cfcfa35068bb6a13a30dd21957007c740e2e58e1
Melanie <melanie.moreau@mail.com>
2017-09-21 10:40:08 +0200

    Fix/plotly resize (#79)
    * chore: make generateTable return the whole object
    
    * fix: resize plot when window is resized
    
    * fix: eslint
    
    * chore: revert to previous generateTable factory
    
    * fix: resize plot when resizing window
    
    * fix: eslint, onResize function
    
    * fix: replace tabs by spaces

--- src/app/shared/plotly/plotly-legacy.module.js
+++ src/app/shared/plotly/plotly-legacy.module.js
@@ -8,38 +8,38 @@ import './plotly.scss';
 // Register plot types
 // Scatter plot is already included in the core
 Plotly.register([
-	PlotlyBox,
-	PlotlyHeatmap,
-	PlotlyBar
+    PlotlyBox,
+    PlotlyHeatmap,
+    PlotlyBar
 ]);
 
 export const PlotlyModule = angular
-	.module('plotly', [])
-	.service('WindowResize', WindowResize)
-	.directive('plot', plotDirective);
+    .module('plotly', [])
+    .service('WindowResize', WindowResize)
+    .directive('plot', plotDirective);
 
 
 function plotDirective() {
-	return {
-		restrict: 'E',
-		scope: {
-			plotData: '=',
-			plotLayout: '=',
-			plotOptions: '='
-		},
-		link(scope, element) {
-			scope.$watchGroup(['plotData', 'plotLayout', 'plotOptions'], ([data, layout, options]) => {
-				data = data || [];
-				Plotly.newPlot(
-					element[0],
-					data,
-					layout,
-					Object.assign({
-						displaylogo: false,
-						showLink: false,
-						showTips: false,
-						sendData: false,
-						modeBarButtonsToRemove: ['sendDataToCloud'],
+    return {
+        restrict: 'E',
+        scope: {
+            plotData: '=',
+            plotLayout: '=',
+            plotOptions: '='
+        },
+        link(scope, element) {
+            scope.$watchGroup(['plotData', 'plotLayout', 'plotOptions'], ([data, layout, options]) => {
+                data = data || [];
+                Plotly.newPlot(
+                    element[0],
+                    data,
+                    layout,
+                    Object.assign({
+                        displaylogo: false,
+                        showLink: false,
+                        showTips: false,
+                        sendData: false,
+                        modeBarButtonsToRemove: ['sendDataToCloud'],
                         modeBarButtonsToAdd: [{
                             // https://github.com/plotly/plotly.js/issues/1576
                             name: 'toLargeImage',
@@ -53,25 +53,41 @@ function plotDirective() {
                                 );
                             }
                         }]
-					}, options || {})
-				);
-			});
+                    }, options || {})
+                );
+            });
 
-			scope.$on('window-resize', () => {
-				Plotly.Plots.resize(element[0]);
-			});
-		}
-	};
+            function onResize() {
+                Plotly.Plots.resize(element[0]);
+            }
+
+            scope.$on('window-resize', () => {
+                onResize();
+            });
+
+            scope.$watch(function () {
+                return {
+                    h: element[0].offsetHeight,
+                    w: element[0].offsetWidth
+                };
+            }, function (newValue, oldValue) {
+                if (angular.equals(newValue, oldValue)) {
+                    return;
+                }
+                onResize();
+            }, true);
+        }
+    };
 }
 
 function WindowResize($window, $rootScope) {
-	let window = angular.element($window);
-	let width = window[0].innerWidth;
+    let window = angular.element($window);
+    let width = window[0].innerWidth;
 
-	angular.element($window).on('resize', () => {
-		let newWidth = window[0].innerWidth;
-		if (width != newWidth) {
-			$rootScope.$broadcast('window-resize', width = newWidth);
-		}
-	});
+    angular.element($window).on('resize', () => {
+        let newWidth = window[0].innerWidth;
+        if (width != newWidth) {
+            $rootScope.$broadcast('window-resize', width = newWidth);
+        }
+    });
 }
