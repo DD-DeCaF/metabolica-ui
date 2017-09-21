bc4a3cd3dff4c6ba64376e677dff09f35fb9fa2e
Mátyás Fodor <matyas.fodor@gmail.com>
2017-09-21 10:39:26 +0200

    Fix missing dependency (#80)

--- src/app/login/login.module.js
+++ src/app/login/login.module.js
@@ -1,7 +1,9 @@
 import angular from 'angular';
 import {LoginComponent} from './login.component';
 
-export const LoginModule = angular.module('login', [])
+export const LoginModule = angular.module('login',[
+        'ui.router'
+    ])
     .component('login', LoginComponent)
     .config(function ($stateProvider) {
         $stateProvider.state('login', {
