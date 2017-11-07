a66fc7f032be54f3f0df5788a12441659f89cd33
Melanie-M <melmor@biosustain.dtu.dk>
2017-11-07 14:34:36 +0100

    chore: register media

--- src/app/media/media.module.ts
+++ src/app/media/media.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MediumComponent } from './medium.component';
 import { MediumListComponent } from './medium-list.component';
+import {RegistryService} from '../registry/registry.service';
 
 @NgModule({
   imports: [
@@ -9,4 +10,19 @@ import { MediumListComponent } from './medium-list.component';
   ],
   declarations: [MediumComponent, MediumListComponent]
 })
-export class MediaModule { }
+export class MediaModule {
+
+  constructor(registry: RegistryService) {
+
+    registry.register('Medium',
+      ['search'],
+      {
+        name: 'medium',
+        pluralName: 'media',
+        route: '/home/media',
+        query: searchText => searchText, // todo: query medium
+        stateParams: medium => ({mediumId: medium.id}),
+        formatAsText: medium => medium.name
+      });
+  }
+}
