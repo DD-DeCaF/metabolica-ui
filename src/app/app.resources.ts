bf2be95e3b98479ad866bd4a77bfd995412e97dc
Pankaj Kumar Singh <pankaj28843@gmail.com>
2017-11-07 14:59:30 +0100

    feat: add more resources from Potion server

--- src/app/app.resources.ts
+++ src/app/app.resources.ts
@@ -1,14 +1,13 @@
 import {Item, PotionResources, Route} from 'potion-client';
 
 
-export class User extends Item {
-  // displayName: string;
-  // firstName: string;
-  // isStaff: boolean;
-  // title: string;
-  // lastName: boolean;
-  // username: string;
+export class Organization extends Item {
+}
 
+export class Group extends Item {
+}
+
+export class User extends Item {
   static current = Route.GET('/me');
   static readSettings = Route.GET('/settings');
   static updateSettings = Route.POST('/settings');
@@ -18,9 +17,34 @@ export class User extends Item {
   shortFullName() {
     return `${(this.firstName || '').split(' ')[0]} ${this.lastName}`;
   }
-
 }
 
+export class PersonalToken extends Item {
+}
+
+export class GroupMembership extends Item {
+}
+
+export class Project extends Item {
+  static readPermissions = Route.GET('/permissions');
+  static readSummary = Route.GET('/summary');
+  static defaultTests = Route.GET('/default-tests');
+  static updateDefaultTests = Route.POST('/update-default-tests');
+}
+
+export class ProjectMembership extends Item {
+}
+
+
 export const resources: PotionResources = {
-  '/user': User
+  '/organization': Organization,
+  '/group': Group,
+  '/user': User,
+  '/personal-token': PersonalToken,
+  '/group-membership': GroupMembership,
+  '/project': Project,
+  '/project-membership': ProjectMembership,
 };
+
+
+
