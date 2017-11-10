423657e2f9808bf5f9f1a18ddfb6253abf1a3ddc
Melanie-M <melmor@biosustain.dtu.dk>
2017-11-10 13:15:30 +0100

    feat: add sorting to medium ingredients table

--- src/app/media/medium.component.ts
+++ src/app/media/medium.component.ts
@@ -5,6 +5,9 @@ import {MatSort} from '@angular/material';
 
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/startWith';
 
 import {Medium} from '../app.resources';
 import {ChemicalEntity} from '../app.resources';
@@ -16,12 +19,32 @@ interface Ingredient {
 }
 
 class IngredientsDataSource extends DataSource<Ingredient> {
-  constructor(private ingredients: Ingredient[]) {
+  data: any;
+
+  constructor(private ingredients: any, private sort: MatSort) {
     super();
   }
 
   connect(): Observable<Ingredient[]> {
-    return Observable.of(this.ingredients);
+    return this.sort.sortChange
+      .startWith(null)
+      .map(() => this.getSortedData());
+  }
+
+  getSortedData(): Ingredient[] {
+    if (!this.sort.active || this.sort.direction === '')  {
+      return this.ingredients;
+    }
+    return this.ingredients.sort((a, b) => {
+      let propertyA: number | string;
+      let propertyB: number | string;
+      switch (this.sort.active) {
+        case 'compound': [propertyA, propertyB] = [a.compound.name, b.compound.name]; break;
+        case 'concentration': [propertyA, propertyB] = [a.concentration, b.concentration]; break;
+        case 'chebi': [propertyA, propertyB] = [a.compound.chebiId, b.compound.chebiId]; break;
+      }
+      return (propertyA < propertyB ? -1 : 1) * (this.sort.direction === 'asc' ? 1 : -1);
+    });
   }
 
   disconnect() {}
@@ -36,24 +59,26 @@ class IngredientsDataSource extends DataSource<Ingredient> {
 export class MediumComponent implements OnInit {
   dataSource: IngredientsDataSource | null;
   displayedColumns = ['compound', 'concentration', 'chebi'];
+  isLoadingIngredients = false;
   medium: any = null;
 
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(route: ActivatedRoute) {
-    route.params.subscribe(params => {
-      const mediumId = params['mediumId'];
-      Medium.fetch(mediumId).then(medium => {
-        this.medium = medium;
-        this.medium.readContents().then(ingredients => {
-          this.dataSource = new IngredientsDataSource(ingredients);
-        });
-      });
-    });
-  }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    // todo: sorting
+    this.route.params.subscribe(params => {
+      const mediumId = params['mediumId'];
+      Medium.fetch(mediumId).then(medium => {
+        this.isLoadingIngredients = true;
+        this.medium = medium;
+        this.medium.readContents()
+          .then(ingredients => {
+            this.dataSource = new IngredientsDataSource(ingredients, this.sort);
+            this.isLoadingIngredients = false;
+          });
+      });
+    });
   }
 
 }
